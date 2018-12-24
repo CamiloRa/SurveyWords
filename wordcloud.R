46186f1137cca265023abfa44960b61935be543b
Camilo Ramirez <camilor@gmail.com>
2018-12-23 23:48:43 -0500

    Fixed data path Fixed Pic name

--- wordcloud.R
+++ wordcloud.R
@@ -11,11 +11,14 @@ library(readr)
 library(tm)
 
 
-stuff <- readr::read_file(./data/raw/Carter_1981.txt)
+data_path <- "./data/raw/"
+data_title <- "Carter_1981.txt"
 picture_width = 1280
 picture_height = 800
 
-picture_name <- paste(comments, time, ".jpg") 
+stuff <- readr::read_file(paste(data_path,data_title,sep=""))
+
+picture_name <- paste(data_title, Sys.Date(), ".jpg", sep="") 
 ld_comments <- Corpus(VectorSource((stuff)))
 ld_comments <- tm_map(ld_comments, PlainTextDocument)
 ld_comments <- tm_map(ld_comments, removePunctuation)
