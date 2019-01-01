5b8cce9bab40072405c78566a5c32552faaaba77
CamiloRa <camilor@gmail.com>
2019-01-01 18:54:02 -0500

    Update wordcloud.R
    Function references source

--- wordcloud.R
+++ wordcloud.R
@@ -10,20 +10,20 @@ library(RColorBrewer)
 library(readr)
 library(tm)
 
-#PaintTheWords
+source:(PaintTheWords.R)
 # function that takes one parameter, and makes the JPEGs from the Corpus after it has been transformed
 
-PaintTheWords <- function(picture_name, picture_width = 1200, picture_height = 800) {
+#PaintTheWords <- function(picture_name, picture_width = 1200, picture_height = 800) {
   
-  jpeg(filename=picture_name, picture_width, picture_height, units = "px")
-  wordcloud(ld_comments,
-            scale=c(3.5,.10),
-            max.words = 500,
-            random.order = FALSE,
-            rot.per = 0,
-            colors= pal)
-  dev.off()
-}
+ # jpeg(filename=picture_name, picture_width, picture_height, units = "px")
+  #wordcloud(ld_comments,
+   #         scale=c(3.5,.10),
+    #        max.words = 500,
+     #       random.order = FALSE,
+      #      rot.per = 0,
+       #     colors= pal)
+  #dev.off()
+#}
 
 
 
