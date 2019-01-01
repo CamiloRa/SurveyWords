a426d565ebd07c9d43617ee16c5be7cfda304c28
CamiloRa <camilor@gmail.com>
2019-01-01 18:52:47 -0500

    Create PaintTheWords.R
    Function here

---
+++ PaintTheWords.R
@@ -0,0 +1,14 @@
+#PaintTheWords
+# function that takes one parameter, and makes the JPEGs from the Corpus after it has been transformed
+
+PaintTheWords <- function(picture_name, picture_width = 1200, picture_height = 800) {
+
+  jpeg(filename=picture_name, picture_width, picture_height, units = "px")
+  wordcloud(ld_comments,
+            scale=c(3.5,.10),
+            max.words = 500,
+            random.order = FALSE,
+            rot.per = 0,
+            colors= pal)
+  dev.off()
+}
