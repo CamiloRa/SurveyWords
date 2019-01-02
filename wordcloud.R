3669305bb55b29bea57153e9b6a88e91b09b6b55
Camilo Ramirez <camilor@gmail.com>
2019-01-01 19:02:00 -0500

    Eliminate commented function

--- wordcloud.R
+++ wordcloud.R
@@ -11,27 +11,12 @@ library(readr)
 library(tm)
 
 source("PaintTheWords.R")
-# function that takes one parameter, and makes the JPEGs from the Corpus after it has been transformed
-
-#PaintTheWords <- function(picture_name, picture_width = 1200, picture_height = 800) {
-  
- # jpeg(filename=picture_name, picture_width, picture_height, units = "px")
-  #wordcloud(ld_comments,
-   #         scale=c(3.5,.10),
-    #        max.words = 500,
-     #       random.order = FALSE,
-      #      rot.per = 0,
-       #     colors= pal)
-  #dev.off()
-#}
-
-
 
 data_path <- "./data/raw/"
 data_title <- "Carter_1981.txt"
 # picture_width = 1280
 # picture_height = 800
-mystopwords <- c(stopwords('english'), 'this', 'the', 'and', 'with', 'that', 'not', 'yes', 'yet')
+mystopwords <- c(stopwords('english'), 'this', 'the', 'and', 'with', 'that', 'not', 'yes', 'yet', 'will')
 pal <- brewer.pal(8, "Accent")
   
 stuff <- readr::read_file(paste(data_path,data_title,sep=""))
